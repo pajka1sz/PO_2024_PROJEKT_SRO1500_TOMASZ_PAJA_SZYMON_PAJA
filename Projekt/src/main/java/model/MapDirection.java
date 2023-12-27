fe8f64816a2bbe9f07e876f8db8497d9c88f1d76
pajka2T <tom16lolo@gmail.com>
2023-12-27 12:37:47 +0100

    MapDirection created with rotate method. Simple checks of it's work in World class.

---
+++ Projekt/src/main/java/model/MapDirection.java
@@ -0,0 +1,38 @@
+package model;
+
+public enum MapDirection {
+    NORTH,
+    NORTH_EAST,
+    EAST,
+    SOUTH_EAST,
+    SOUTH,
+    SOUTH_WEST,
+    WEST,
+    NORTH_WEST;
+
+    public MapDirection rotate(int rotation) {
+        int baseValue = switch(this) {
+            case NORTH -> 0;
+            case NORTH_EAST -> 1;
+            case EAST -> 2;
+            case SOUTH_EAST -> 3;
+            case SOUTH -> 4;
+            case SOUTH_WEST -> 5;
+            case WEST -> 6;
+            case NORTH_WEST -> 7;
+        };
+
+        int result = (baseValue + rotation) % 8;
+        return switch (result) {
+            case 0 -> NORTH;
+            case 1 -> NORTH_EAST;
+            case 2 -> EAST;
+            case 3 -> SOUTH_EAST;
+            case 4 -> SOUTH;
+            case 5 -> SOUTH_WEST;
+            case 6 -> WEST;
+            case 7 -> NORTH_WEST;
+            default -> null;
+        };
+    }
+}
