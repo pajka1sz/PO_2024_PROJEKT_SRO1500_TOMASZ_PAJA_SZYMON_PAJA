bd898175cb8aa22a975e0f8fc9cbfccc717ce366
pajka2T <tom16lolo@gmail.com>
2023-12-28 11:09:38 +0100

    MapDirection toUnitVector() method created.

--- Projekt/src/main/java/model/MapDirection.java
+++ Projekt/src/main/java/model/MapDirection.java
@@ -35,4 +35,17 @@ public enum MapDirection {
             default -> null;
         };
     }
+
+    public Vector2d toUnitVector() {
+        return switch(this) {
+            case NORTH -> new Vector2d(0, 1);
+            case NORTH_EAST -> new Vector2d(1, 1);
+            case EAST -> new Vector2d(1, 0);
+            case SOUTH_EAST -> new Vector2d(1, -1);
+            case SOUTH -> new Vector2d(0, -1);
+            case SOUTH_WEST -> new Vector2d(-1, -1);
+            case WEST -> new Vector2d(-1, 0);
+            case NORTH_WEST -> new Vector2d(-1, 1);
+        };
+    }
 }
