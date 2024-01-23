ea1d36e1ffc4ce744968b2142c18e331f425aece
pajka1sz <szymusek45juve@gmail.com>
2024-01-23 23:13:11 +0100

    Build gradle changed

--- Projekt/src/main/java/World.java
+++ Projekt/src/main/java/World.java
@@ -3,29 +3,8 @@ import model.genom.NormalGenom;
 
 public class World {
     public static void main(String[] args) {
-        run(args);
-        NormalGenom normalGenom = new NormalGenom(8, 3, 100, "24571011",
-                50, "03357742");
-        //System.out.println(normalGenom.getGenom());
-//        System.out.println(normalGenom.getGen() + " " + normalGenom.getGen());
-        JumpingGenom jumpingGenom = new JumpingGenom(8, 3, 100, "24571011",
-                50, "03357742");
-        System.out.println(jumpingGenom.getGenom());
-        System.out.println(jumpingGenom.getGen() + " " + jumpingGenom.getGen() + " " + jumpingGenom.getGen() + " " +
-                jumpingGenom.getGen() + " " + jumpingGenom.getGen() + " " + jumpingGenom.getGen() + " " +
-                jumpingGenom.getGen() + " " + jumpingGenom.getGen() + " " + jumpingGenom.getGen() + " " +
-                jumpingGenom.getGen());
+
     }
 
-    public static void run(String[] args) {
-        String message;
-        for (String arg: args) {
-            message = switch (arg) {
-                case "0" -> "Zwierzak idzie na północ.";
-                case "1" -> "Zwierzak idzie na północny-wschód.";
-                default -> "Zły kierunek!";
-            };
-            System.out.println(message);
-        }
-    }
+
 }
